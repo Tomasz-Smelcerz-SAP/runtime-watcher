02abc3a5b892dbdfc63670d2bba1d93ce24dbd74
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2023-11-15 00:37:02 +0100

    Add release workflow

--- runtime-watcher/scripts/release/get_last_release.sh
+++ runtime-watcher/scripts/release/get_last_release.sh
@@ -1,11 +1,9 @@
-!/usr/bin/env bash
+#!/usr/bin/env bash
 
 set -o nounset
 set -o errexit
 set -E
 set -o pipefail
-
-RELEASE_TAG=$1
 
 GITHUB_URL=https://api.github.com/repos/kyma-project/runtime-watcher
 GITHUB_AUTH_HEADER="Authorization: Bearer ${GITHUB_TOKEN}"
@@ -15,6 +13,8 @@ CURL_RESPONSE=$(curl -L \
   -H "${GITHUB_AUTH_HEADER}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "${GITHUB_URL}"/releases/latest) 
+
+echo RESULT: $?
 
 # return the id of the release draft
 echo "$CURL_RESPONSE"
