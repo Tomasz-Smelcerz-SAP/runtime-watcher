f9faadbacbd44e65cd0fcd0e634d3c5b71dd917c
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2023-11-17 14:15:37 +0100

    Add release workflow

---
+++ runtime-watcher/scripts/release/wait_for_image.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -E
+set -o pipefail
+
+DOCKER_IMAGE=$1
+
+for i in {1..30} ; do
+    if $(docker manifest inspect $1 > /dev/null 2>&1); then
+	    exit 0
+    fi
+    echo "Attempt $i: Docker image: $DOCKER_IMAGE doesn't exist"
+    sleep 30
+done
+
+echo "Fail: Docker image: $DOCKER_IMAGE doesn't exist"
+exit 1
+
